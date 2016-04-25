e9a6be3351f917e2649c248ac9443405c2121173
Leslie Zhai <xiangzhai83@gmail.com>
2016-04-25 10:12:17 +0800

    Update header nav

--- header.php
+++ header.php
@@ -41,6 +41,9 @@ echo '
       <li><a href="https://isoft-linux.org" ' . current_page_cb(isset($_GET['p']) ? $_GET['p'] == 'index' : true) . '>首页</a></li>
       <li><a href="https://bugs.isoft-linux.org" ' . current_page_cb(isset($_GET['p']) ? $_GET['p'] == 'bug' : false) . '>缺陷管理</a></li>
       <li><a href="https://forum.isoft-linux.org" ' . current_page_cb(isset($_GET['p']) ? $_GET['p'] == 'forum' : false) . '>论坛</a></li>
+      <!--<li><a href="#">维基百科</a></li>-->
+      <li><a href="https://isoft-linux.org/index.php/projects/">项目列表</a></li>
+      <li><a href="https://isoft-linux.org/index.php/aboutus/">关于我们</a></li>
       <li>&nbsp</li>
     </ul>
   </div>
